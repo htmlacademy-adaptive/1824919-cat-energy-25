2534d309cdefac97c2ba23668dd4184bf0d83112
Илья Горшенев <gorschenyov.ilya2014@yandex.ru>
2023-02-28 19:52:55 +0300

    pages

---
+++ docks/js/script.js
@@ -0,0 +1,12 @@
+let navMain = document.querySelector('.main-header');
+let navToggle = document.querySelector('.main-header__toggle');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-header--closed')) {
+    navMain.classList.remove('main-header--closed');
+    navMain.classList.add('main-header--opened');
+  } else {
+    navMain.classList.add('main-header--closed');
+    navMain.classList.remove('main-header--opened');
+  } 
+});
